e211716eb213fc7875f4dccb958593dd4e25e33e
Johnson Joseph <jhonsonj88@gmail.com>
2019-08-11 20:35:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function driversWithRevenueOver(drivers, quantity) {
  return drivers.filter(function (driver) {
   return driver.revenue > quantity;
- }); 
+ });
 }
 
  function driverNamesWithRevenueOver(drivers, quantity) {
